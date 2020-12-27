e543cc1ab814bd0043bcee39ba6e2323e71e2c73
vamsikrishna-api <75066513+vamsikrishna-api@users.noreply.github.com>
2020-12-27 21:19:36 +0530

    feat: dd12 changes

--- src/main/java/com/beis/subsidy/award/transperancy/dbpublishingservice/service/AwardService.java
+++ src/main/java/com/beis/subsidy/award/transperancy/dbpublishingservice/service/AwardService.java
@@ -10,7 +10,6 @@ import java.util.Locale;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import com.beis.subsidy.award.transperancy.dbpublishingservice.repository.GrantingAuthorityRepository;
 import com.beis.subsidy.award.transperancy.dbpublishingservice.repository.SubsidyMeasureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
