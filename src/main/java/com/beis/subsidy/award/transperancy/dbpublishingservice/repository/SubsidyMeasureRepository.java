fe5f9259c4fbdb160f16b075d1994d316388ecb4
KOTLAPRA2711 <prashanth.kotla@cognizant.com>
2020-12-27 21:01:43 +0530

    feat:adding the repositories which were required

---
+++ src/main/java/com/beis/subsidy/award/transperancy/dbpublishingservice/repository/SubsidyMeasureRepository.java
@@ -0,0 +1,9 @@
+package com.beis.subsidy.award.transperancy.dbpublishingservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.beis.subsidy.award.transperancy.dbpublishingservice.model.SubsidyMeasure;
+
+public interface SubsidyMeasureRepository extends JpaRepository<SubsidyMeasure, String> {
+
+}
