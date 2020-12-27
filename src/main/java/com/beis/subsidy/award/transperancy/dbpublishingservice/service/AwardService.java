02922123e37dcfe9af71d9b9fab7a08685683ccd
KOTLAPRA2711 <prashanth.kotla@cognizant.com>
2020-12-27 21:25:45 +0530

    feat:adding the GrantingAuthorityRepository

--- src/main/java/com/beis/subsidy/award/transperancy/dbpublishingservice/service/AwardService.java
+++ src/main/java/com/beis/subsidy/award/transperancy/dbpublishingservice/service/AwardService.java
@@ -10,6 +10,7 @@ import java.util.Locale;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.beis.subsidy.award.transperancy.dbpublishingservice.repository.GrantingAuthorityRepository;
 import com.beis.subsidy.award.transperancy.dbpublishingservice.repository.SubsidyMeasureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
