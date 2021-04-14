16bbe892c8e20d08dea03802024c9bfa778f097d
KOTLAPRA2711 <prashanth.kotla@cognizant.com>
2021-04-14 13:15:28 +0530

    feat:updated the audit log message for update award scenario.

--- src/main/java/com/beis/subsidy/award/transperancy/dbpublishingservice/controller/AddAwardController.java
+++ src/main/java/com/beis/subsidy/award/transperancy/dbpublishingservice/controller/AddAwardController.java
@@ -146,7 +146,7 @@ public class AddAwardController {
 
 				//Audit entry
 				StringBuilder eventMsg = new StringBuilder("Award ").append(updatedAward.getAwardNumber())
-						.append("is updated to ").append(awardInputRequest.getStatus());
+						.append(" is updated to ").append(awardInputRequest.getStatus());
 				ExcelHelper.saveAuditLogForUpdate(userPrincipleObj, "Update Award", updatedAward.getAwardNumber().toString()
 						,eventMsg.toString(),auditLogsRepository);
 			}
